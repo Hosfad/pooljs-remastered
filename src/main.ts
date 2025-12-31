696f8811d782211d83b3a926888b4d62b50d4567
hosfad <firassabea1@outlook.com>
2025-12-31 17:45:48 +0100

    Made canvas fullscreen

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,6 @@ import * as Phaser from "phaser";
 import { PoolGameScene } from "./scenes/pool-game-scene";
 import { PoolPreLoadScene } from "./scenes/pool-preload-scene";
 
-const BASE_WIDTH = 1800;
-const BASE_HEIGHT = 960;
-
 const gameConfig: Phaser.Types.Core.GameConfig = {
     type: Phaser.CANVAS,
     pixelArt: true,
@@ -12,11 +9,12 @@ const gameConfig: Phaser.Types.Core.GameConfig = {
     backgroundColor: "#1a472a",
 
     scale: {
-        mode: Phaser.Scale.FIT,
+        mode: Phaser.Scale.RESIZE,
         autoCenter: Phaser.Scale.CENTER_BOTH,
-        width: BASE_WIDTH,
-        height: BASE_HEIGHT,
+        width: "100%",
+        height: "100%",
     },
+
     input: {
         keyboard: true,
         mouse: true,
@@ -27,10 +25,5 @@ const gameConfig: Phaser.Types.Core.GameConfig = {
 };
 
 window.onload = () => {
-    const game = new Phaser.Game(gameConfig);
-
-    // Optional: handle dynamic resize
-    window.addEventListener("resize", () => {
-        game.scale.resize(BASE_WIDTH, BASE_HEIGHT);
-    });
+    new Phaser.Game(gameConfig);
 };
